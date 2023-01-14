291a95d82c607e4604680d5bbcc79acfdaacb5ca
linxb <448923515@qq.com>
2023-01-14 14:08:53 +0800

    code review

--- pkg/errcode/common_code.go
+++ pkg/errcode/common_code.go
@@ -1,7 +1,7 @@
 package errcode
 
 var (
-	Success                   = NewError(0, "成功")
+	Success                   = NewError(0, "操作成功")
 	ServerError               = NewError(10000000, "服务内部错误")
 	InvalidParams             = NewError(10000001, "入参错误")
 	NotFound                  = NewError(10000002, "找不到请求资源")
